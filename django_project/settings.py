0808ff98596f8bce9340689d276e870350a702f9
raffayell <rafayel.mar@gmail.com>
2025-03-21 14:57:20 +0400

    corsheader added

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     # 3rd-party apps
     "rest_framework",
+    "corsheaders",
 
     # local apps
     'accounts.apps.AccountsConfig',
@@ -43,12 +44,20 @@ REST_FRAMEWORK = { # new
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'django_project.urls'
 
